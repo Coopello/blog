786768ddae899ddb72c537dcaeaf4e58c6c3f1fb
github akito-10 <akt.fukuda@gmail.com>
2022-12-10 02:07:46 +0900

    🔧 add tailwind custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        custom: {
+          black: "#222222",
+          red: "#FF7971",
+          blue: "#717FFF",
+          green: "#43DC49",
+          orange: "#FFBA69",
+          purple: "#A055FF",
+          gray: "#C9C9C9",
+          defaultGray: "#F5F5F5",
+          coopelloBlue: "#5AC8D8",
+        },
+      },
+    },
   },
   important: true,
   plugins: [
