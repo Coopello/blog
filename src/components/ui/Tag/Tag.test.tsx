4e1598e3ad844f6fa4e03e62af5e84bfa7b86e1a
github akito-10 <akt.fukuda@gmail.com>
2022-12-11 19:22:56 +0900

    ✅ Tagコンポーネントのカバレッジを向上

--- src/components/ui/Tag/Tag.test.tsx
+++ src/components/ui/Tag/Tag.test.tsx
@@ -4,8 +4,13 @@ import { Tag } from "./";
 // ______________________________________________________
 //
 describe("ui/Tag", () => {
-  it("Snap Shot", () => {
+  it("Snap Shot Chip", () => {
     const component = renderer.create(<Tag type="chip" />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it("Snap Shot outline Chip", () => {
+    const component = renderer.create(<Tag type="chip" outline />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
