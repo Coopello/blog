19b50cba57bfcadfe637db4475c9d1d01289be0a
akito <63759669+akito-10@users.noreply.github.com>
2023-01-08 01:59:44 +0900

    hygenのテンプレートを編集 (#2)
    * 🔨 edit hygen template
    
    * 🔨 hygen pageコマンド削除

--- .hygen/new/fc/index.js
+++ .hygen/new/fc/index.js
@@ -1,9 +1,12 @@
-//
-// npm run new:sfc -- --tag=p
-//
 module.exports = {
   prompt: ({ inquirer, args }) => {
     const questions = [
+      {
+        type: "select",
+        name: "dir_type",
+        message: "Which components directory?",
+        choices: ["page", "ui"],
+      },
       {
         type: "input",
         name: "component_name",
@@ -16,12 +19,15 @@ module.exports = {
       },
     ];
     return inquirer.prompt(questions).then((answers) => {
-      const { component_name, have_props } = answers;
-      const path = `ui/${
-        component_name.charAt(0).toUpperCase() + component_name.slice(1)
+      const { component_name, dir_type, have_props } = answers;
+      const file_name = `${component_name
+        .charAt(0)
+        .toUpperCase()}${component_name.slice(1)}${
+        dir_type === "page" ? ".page" : ""
       }`;
+      const path = `${dir_type}/${file_name}`;
       const abs_path = `src/components/${path}`;
-      const type_annotate = have_props ? "React.FC<Props>" : "React.FC";
+      const type_annotate = have_props ? "FC<Props>" : "FC";
       const props = have_props ? "({})" : "()";
       const tag = args.tag ? args.tag : "div";
       return { ...answers, path, abs_path, type_annotate, props, tag };
--- .hygen/new/fc/index.ts.ejs.t
+++ .hygen/new/fc/index.ts.ejs.t
@@ -1,4 +1,4 @@
 ---
-to: <%= abs_path %>/index.ts
+to: <%= abs_path %>/index.tsx
 ---
 export { <%= h.changeCase.pascal(component_name) %> } from "./<%= h.changeCase.pascal(component_name) %>";
