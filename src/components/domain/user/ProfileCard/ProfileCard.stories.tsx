c52e88ec2a1ebba95afa98513cd4d39b86a094c5
github akito-10 <akt.fukuda@gmail.com>
2023-01-08 16:50:20 +0900

    💄 ProfileCardコンポーネント作成

---
+++ src/components/domain/user/ProfileCard/ProfileCard.stories.tsx
@@ -0,0 +1,21 @@
+import type { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { ProfileCard } from ".";
+
+export default { title: "domain/user/ProfileCard" } as ComponentMeta<
+  typeof ProfileCard
+>;
+
+export const initial: ComponentStory<typeof ProfileCard> = (args) => (
+  <ProfileCard {...args} />
+);
+
+initial.args = {
+  imageUrl: "https://placehold.jp/150x150.png",
+  myUrl: {
+    twitter: "https://twitter.com/akt_prs10",
+    github: "https://github.com/akito-10",
+  },
+  name: "Akito Fukuda",
+  description: "Web Dev",
+};
