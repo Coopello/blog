61b107ffe3cb62c376e9b7fd38c4b230dc6ca45d
github akito-10 <akt.fukuda@gmail.com>
2023-02-11 14:59:04 +0900

    💄 Header作成

---
+++ src/components/ui/Header/Header.stories.tsx
@@ -0,0 +1,9 @@
+import type { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { Header } from "./";
+
+export default { title: "ui/Header" } as ComponentMeta<
+  typeof Header
+>;
+
+export const Initial: ComponentStory<typeof Header> = (args) => <Header {...args} />;
---
+++ src/components/ui/Header/Header.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import type { FC } from "react";
+/**
+ * @package
+ */
+export const Header: FC = () => {
+  return (
+    <header className="flex items-center px-8 py-2">
+      <Image
+        src="/static/coopeblog_logo.png"
+        alt="こぺブログロゴ"
+        width={60}
+        height={60}
+      />
+      <h1 className="text-3xl font-bold">Coope Blog</h1>
+    </header>
+  );
+};
