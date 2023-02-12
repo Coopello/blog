7c55a9ca6d5b410208a02b0b17b2225b670c896d
Kaito-Dogi <kendobu0405@gmail.com>
2023-02-12 19:00:46 +0900

    ♻️ React.ReactNode -> ReactNode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,9 @@
 import "src/styles/globals.css";
 
+import type { ReactNode } from "react";
 import { Header } from "src/components/ui/Header";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html>
       <head />
