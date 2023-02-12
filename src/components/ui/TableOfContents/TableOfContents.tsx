262bb20313f2038b9277dba01562716d38baf607
github akito-10 <akt.fukuda@gmail.com>
2023-02-12 17:54:31 +0900

    ♻️ nav消去

--- src/components/ui/TableOfContents/TableOfContents.tsx
+++ src/components/ui/TableOfContents/TableOfContents.tsx
@@ -15,22 +15,20 @@ export const TableOfContents: FC<Props> = ({ contentId, contents }) => {
   const { targetTocDataVal } = useTableOfContents(contentId);
 
   return (
-    <nav>
-      <ul className="flex flex-col gap-2">
-        {contents.map((content, i) => {
-          return (
-            <li key={i} className="h-full w-full">
-              <a
-                data-val={targetTocDataVal}
-                className="box-border block h-full w-full px-4 text-xl hover:bg-black/5"
-                href={`#${content}`}
-              >
-                {content}
-              </a>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
+    <ul className="flex flex-col gap-2">
+      {contents.map((content, i) => {
+        return (
+          <li key={i} className="h-full w-full">
+            <a
+              data-val={targetTocDataVal}
+              className="box-border block h-full w-full px-4 text-xl hover:bg-black/5"
+              href={`#${content}`}
+            >
+              {content}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
