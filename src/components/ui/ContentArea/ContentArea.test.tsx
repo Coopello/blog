e9011fdd956ecf1dd556b17fff53251aeae94cf3
Kaito-Dogi <kendobu0405@gmail.com>
2023-02-12 19:56:26 +0900

    ✅

--- src/components/ui/ContentArea/ContentArea.test.tsx
+++ src/components/ui/ContentArea/ContentArea.test.tsx
@@ -4,7 +4,9 @@ import { ContentArea } from ".";
 
 describe("ui/ContentArea", () => {
   it("Snap Shot", () => {
-    const component = renderer.create(<ContentArea />);
+    const component = renderer.create(
+      <ContentArea id="test" content="<h1>test</h1>" />
+    );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
