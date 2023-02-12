527ef389dcd7b0693cd82a7f477c8538f08dede0
Kaito-Dogi <kendobu0405@gmail.com>
2023-02-12 19:55:51 +0900

    🔧 storyboard

--- src/components/ui/ContentArea/ContentArea.stories.tsx
+++ src/components/ui/ContentArea/ContentArea.stories.tsx
@@ -7,3 +7,8 @@ export default { title: "ui/ContentArea" } as ComponentMeta<typeof ContentArea>;
 export const Initial: ComponentStory<typeof ContentArea> = (args) => (
   <ContentArea {...args} />
 );
+
+Initial.args = {
+  id: "test",
+  content: "<h1>test</h1>",
+};
