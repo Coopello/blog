5c350bf15a20461a843f64754802b626e4a3a257
github akito-10 <akt.fukuda@gmail.com>
2023-02-18 17:10:09 +0900

    ♻️ タグコンポーネントをリファクタ

--- src/components/ui/Tag/Tag.tsx
+++ src/components/ui/Tag/Tag.tsx
@@ -1,78 +1,47 @@
 "use client";
 
-import type { FC } from "react";
-
-type TagType =
-  | "chip"
-  | "frontend"
-  | "backend"
-  | "mobile"
-  | "infrastructure"
-  | "design"
-  | "us";
+import type { CSSProperties, FC } from "react";
+import { useState } from "react";
 
 type Props = {
-  type: TagType;
+  tagInfo: {
+    color: `#${string}`;
+    text: string;
+  };
   filled?: boolean;
   onClick?: () => void;
 };
 
-const TAG_ASSET = {
-  chip: {
-    text: "Chip",
-    color: "#222222",
-  },
-  frontend: {
-    text: "Frontend",
-    color: "#FF7971",
-  },
-  backend: {
-    text: "Backend",
-    color: "#717FFF",
-  },
-  mobile: {
-    text: "Mobile",
-    color: "#43DC49",
-  },
-  infrastructure: {
-    text: "Infrastructure",
-    color: "#FFBA69",
-  },
-  design: {
-    text: "Design",
-    color: "#A055FF",
-  },
-  us: {
-    text: "Us",
-    color: "#5AC8D8",
-  },
-} as const;
-
 /**
  * @package
  */
-export const Tag: FC<Props> = ({ filled = false, onClick, type }) => {
-  const { color, text } = TAG_ASSET[type];
-  // classNameベースで動的なスタイリングができないので、仕方なくstyle
-  const styles = filled
+export const Tag: FC<Props> = ({ filled = false, onClick, tagInfo }) => {
+  // inline-styleで対応しているため、stateを使用してhoverを制御。
+  const [isHover, setIsHover] = useState(false);
+  const { color, text } = tagInfo;
+  const Element = onClick ? "button" : "span";
+
+  // レンダリングの都合上、classNameを動的に設定できないため、inline-styleで対応。
+  const tagStyle: CSSProperties = filled
     ? {
         color: "white",
         backgroundColor: color,
+        opacity: isHover ? 0.7 : 1.0,
       }
     : {
-        border: `1px solid ${color}`,
         color,
+        backgroundColor: isHover ? "rgba(0, 0, 0, 0.1)" : "white",
       };
-
-  const Element = onClick ? "button" : "span";
 
   return (
     <Element
-      className="rounded-full px-4 py-2 text-xs font-bold"
-      style={styles}
+      className={"rounded-sm px-3 py-1 text-sm"}
+      style={tagStyle}
       onClick={onClick}
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
     >
-      {text}
+      #{text}
     </Element>
   );
 };
