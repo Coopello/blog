ec295f581d6a11a4ad277e7cdff22ed0a75d64f7
github akito-10 <akt.fukuda@gmail.com>
2023-02-18 17:30:15 +0900

    ✅

--- src/components/ui/Tag/Tag.test.tsx
+++ src/components/ui/Tag/Tag.test.tsx
@@ -5,19 +5,42 @@ import { Tag } from "./";
 //
 describe("ui/Tag", () => {
   it("Snap Shot Chip", () => {
-    const component = renderer.create(<Tag type="chip" filled />);
+    const component = renderer.create(
+      <Tag
+        tagInfo={{
+          text: "chip",
+          color: "#000",
+        }}
+        filled
+      />
+    );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
   it("Snap Shot outline Chip", () => {
-    const component = renderer.create(<Tag type="chip" />);
+    const component = renderer.create(
+      <Tag
+        tagInfo={{
+          text: "chip",
+          color: "#000",
+        }}
+      />
+    );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
   it("Snap Shot button Chip", () => {
     const onClick = jest.fn();
 
-    const component = renderer.create(<Tag type="chip" onClick={onClick} />);
+    const component = renderer.create(
+      <Tag
+        tagInfo={{
+          text: "chip",
+          color: "#000",
+        }}
+        onClick={onClick}
+      />
+    );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
