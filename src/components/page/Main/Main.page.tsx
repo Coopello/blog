715c8b9a851b3dd9250ad839ee6de54f950cdce9
github akito-10 <akt.fukuda@gmail.com>
2023-02-18 17:13:21 +0900

    💄 ダミー

--- src/components/page/Main/Main.page.tsx
+++ src/components/page/Main/Main.page.tsx
@@ -12,13 +12,11 @@ import { Tag } from "src/components/ui/Tag";
 export const Main: FC = () => {
   return (
     <div>
-      <Tag type="chip" filled onClick={() => console.log()} />
-      <Tag type="frontend" filled onClick={() => console.log()} />
-      <Tag type="backend" filled />
-      <Tag type="mobile" filled />
-      <Tag type="infrastructure" filled />
-      <Tag type="design" filled />
-      <Tag type="us" filled />
+      <Tag
+        tagInfo={{ color: "#000", text: "chip" }}
+        filled
+        onClick={() => console.log()}
+      />
       <ProfileCard
         imageUrl="https://placehold.jp/150x150.png"
         myLinks={{
