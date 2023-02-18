e0e352de831a692b2fc92f43061dbb1cbed8f579
github akito-10 <akt.fukuda@gmail.com>
2023-02-18 18:04:28 +0900

    💄 Header

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -7,14 +7,14 @@ import type { FC } from "react";
  */
 export const Header: FC = () => {
   return (
-    <header className="flex items-center px-8 py-2">
+    <header className="flex items-center p-4">
       <Image
         src="/static/coopeblog_logo.png"
         alt="こぺブログロゴ"
         width={60}
         height={60}
       />
-      <h1 className="text-3xl font-bold">Coope Blog</h1>
+      <h1 className="text-4xl">Coope Blog</h1>
     </header>
   );
 };
