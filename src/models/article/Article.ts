ed492d125a873478541a7b57809707196e615332
github akito-10 <akt.fukuda@gmail.com>
2023-02-21 00:20:54 +0900

    🏷️ Category

--- src/models/article/Article.ts
+++ src/models/article/Article.ts
@@ -1,15 +1,8 @@
+import type { Category } from "src/models/category";
+
 /**
  * @package
  */
 export type Article = {
-  articleTags: ArticleTagType[];
+  articleTags: Category[];
 };
-
-type ArticleTagType =
-  | "chip"
-  | "frontend"
-  | "backend"
-  | "mobile"
-  | "infrastructure"
-  | "design"
-  | "us";
---
+++ src/models/category/Category.ts
@@ -0,0 +1,11 @@
+/**
+ * @package
+ */
+export type Category =
+  | "chip"
+  | "frontend"
+  | "backend"
+  | "mobile"
+  | "infrastructure"
+  | "design"
+  | "us";
