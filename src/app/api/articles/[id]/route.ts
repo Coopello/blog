f325dfa1770fd42bea7f18e8d5931688d8a97093
github akito-10 <akt.fukuda@gmail.com>
2023-03-06 21:18:12 +0900

    ✨ axiosで書き換え

--- src/app/api/articles/[id]/route.ts
+++ src/app/api/articles/[id]/route.ts
@@ -1,18 +1,12 @@
 import * as cheerio from "cheerio";
 import hljs from "highlight.js";
 import { NextResponse } from "next/server";
+import { microCMSApiServer } from "src/libs/apiClient";
 
 export async function GET(_: Request, { params }: { params: { id: string } }) {
   const id = params.id;
 
-  // FIXME: axios 導入後に移動する
-  const res = await fetch(`https://kaito-dogi.microcms.io/api/v1/blogs/${id}`, {
-    method: "GET",
-    headers: {
-      "X-MICROCMS-API-KEY": process.env.MICRO_CMS_API_KEY || "",
-    },
-  });
-  const data = await res.json();
+  const { data } = await microCMSApiServer.get(`/blogs/${id}`);
 
   const $ = cheerio.load(data.content, null, false);
   $("pre code").each((_, elm) => {
