097a02874f189651e52f87a91e2e559a7a47455a
Kaito-Dogi <kendobu0405@gmail.com>
2023-03-24 02:51:17 +0900

    ✨

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,11 @@
 export default function Head() {
   return (
     <>
-      <title></title>
+      <title>Coope Blog</title>
+      <meta
+        name="description"
+        content="みんながよく知る企業をめざしたいエンジニア必見！有名企業エンジニアによる「未経験からでも悩まない」ノウハウがここに。"
+      />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
     </>
