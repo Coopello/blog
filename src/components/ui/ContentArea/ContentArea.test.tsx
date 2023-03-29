2fdcd4fa1a362d646e40bce5b283ccfd43308f3d
github akito-10 <akt.fukuda@gmail.com>
2023-03-30 01:45:03 +0900

    ✅

--- src/components/ui/ContentArea/ContentArea.test.tsx
+++ src/components/ui/ContentArea/ContentArea.test.tsx
@@ -5,7 +5,13 @@ import { ContentArea } from ".";
 describe("ui/ContentArea", () => {
   it("Snap Shot", () => {
     const component = renderer.create(
-      <ContentArea id="test" content="<h1>test</h1>" />
+      <ContentArea
+        id="test"
+        content="<h1>test</h1>"
+        title="test"
+        tags={[{ name: "test" }]}
+        categoryColor="#000"
+      />
     );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
