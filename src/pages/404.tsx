43a357564db5b31dbb28728be6245bcf2b0f7478
Kaito-Dogi <kendobu0405@gmail.com>
2023-04-12 01:43:53 +0900

    💄 404ページを表示できないか実験中

---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+// TODO: pages 配下に404を置くことで404ページを表示できないか試しているので、不要になったら削除する
+export default function NotFound() {
+  return (
+    <>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+    </>
+  );
+}
