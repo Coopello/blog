b0d7e8c5a00aee609a8814e0da41e45ed02a98a0
github akito-10 <akt.fukuda@gmail.com>
2023-04-12 20:57:13 +0900

    ✅

--- src/components/ui/Tag/Tag.test.tsx
+++ src/components/ui/Tag/Tag.test.tsx
@@ -1,9 +1,12 @@
+import { render, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import renderer from "react-test-renderer";
 
 import { Tag } from "./";
-// ______________________________________________________
-//
+
 describe("ui/Tag", () => {
+  const onClick = jest.fn();
+
   it("Snap Shot Chip", () => {
     const component = renderer.create(
       <Tag color="#E10718" text="front-end" filled />
@@ -16,13 +19,52 @@ describe("ui/Tag", () => {
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
-  it("Snap Shot button Chip", () => {
-    const onClick = jest.fn();
 
+  it("Snap Shot button Chip", () => {
     const component = renderer.create(
       <Tag color="#E10718" text="front-end" onClick={onClick} />
     );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  describe("ボタンとしてレンダリングされた場合に、", () => {
+    it("filled=false のホバー時のインタラクションが適切に動作している。", async () => {
+      const { getByRole } = render(
+        <Tag color="#E10718" text="front-end" onClick={onClick} />
+      );
+
+      const tagButton = getByRole("button");
+      expect(tagButton.style.backgroundColor).toBe("transparent");
+
+      userEvent.hover(tagButton);
+      await waitFor(() => {
+        expect(tagButton.style.backgroundColor).toBe("rgba(0, 0, 0, 0.1)");
+      });
+
+      userEvent.unhover(tagButton);
+      await waitFor(() => {
+        expect(tagButton.style.backgroundColor).toBe("transparent");
+      });
+    });
+
+    it("filled=true のホバー時のインタラクションが適切に動作している。", async () => {
+      const { getByRole } = render(
+        <Tag color="#E10718" text="front-end" onClick={onClick} filled />
+      );
+
+      const tagButton = getByRole("button");
+      expect(tagButton.style.opacity).toBe("1");
+
+      userEvent.hover(tagButton);
+      await waitFor(() => {
+        expect(tagButton.style.opacity).toBe("0.7");
+      });
+
+      userEvent.unhover(tagButton);
+      await waitFor(() => {
+        expect(tagButton.style.opacity).toBe("1");
+      });
+    });
   });
 });
