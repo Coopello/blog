3693aaaa56373c0a5fdd948b08851fcbb06416f0
Kaito-Dogi <kendobu0405@gmail.com>
2023-04-13 22:21:45 +0900

    ⚰️ 不要な404ページを削除

--- src/pages/404.tsx
+++
@@ -1,9 +0,0 @@
-// TODO: pages 配下に404を置くことで404ページを表示できないか試しているので、不要になったら削除する
-export default function NotFound() {
-  return (
-    <>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-    </>
-  );
-}
