4adee1c7c507cbf3a1d416bfc80f771828616935
Kaito-Dogi <kendobu0405@gmail.com>
2023-04-13 23:58:57 +0900

    💄 NotFoundPage を整理

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,5 @@
-// TODO: app 配下にnot-found.tsxを置くことで404ページを表示できないか試しているので、不要になったら削除する
-// ドキュメント的にはこの方法が正しいとされている
-export default function NotFound() {
-  return (
-    <>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-    </>
-  );
+import { NotFound } from "src/components/page/NotFound";
+
+export default function NotFoundPage() {
+  return <NotFound />;
 }
--- src/components/page/NotFound/NotFound.page.tsx
+++ src/components/page/NotFound/NotFound.page.tsx
@@ -1,8 +1,24 @@
+import Image from "next/image";
 import type { FC } from "react";
+import { Button } from "src/components/ui/Button";
 
 /**
  * @package
  */
 export const NotFound: FC = () => {
-  return <div></div>;
+  return (
+    <div className="grid min-h-[calc(100vh_-_60px)] w-full place-content-center gap-12 pb-20 sm:min-h-[calc(100vh_-_112px)]">
+      <div>
+        <Image
+          className="mb-6"
+          src="/static/error_icon.svg"
+          alt="エラーアイコン"
+          width={256}
+          height={256}
+        />
+        <h2 className="text-2xl text-custom-sky">ページが見つかりません。</h2>
+      </div>
+      <Button>トップページに戻る</Button>
+    </div>
+  );
 };
