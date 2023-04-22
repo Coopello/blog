7643f5741a8d9159f1edc8ebc1ccfefec5a2e5ed
github akito-10 <akt.fukuda@gmail.com>
2023-04-23 00:54:57 +0900

    🏷️ RequestInit 型拡張

--- declarations.d.ts
+++ declarations.d.ts
@@ -3,3 +3,9 @@ declare module "*.css" {
   const content: Record<string, string>;
   export default content;
 }
+
+declare global {
+  interface RequestInit {
+    next?: NextFetchRequestConfig;
+  }
+}
