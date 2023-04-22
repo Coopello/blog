777c98df7161428319569a336d22a73297c63b81
Kaito-Dogi <kendobu0405@gmail.com>
2023-04-22 18:21:58 +0900

    ✨ 人気記事を取得するよう fetch を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { Home } from "src/components/page/Home";
+import type { Article } from "src/models/article";
 
-export default function HomePage() {
-  return <Home />;
+type Response = {
+  contents: Article[];
+};
+
+const getPopularBlogsData = async () => {
+  const res = await fetch(`${process.env.MICRO_CMS_API_URL}/blogs?orders=-pv`, {
+    next: { revalidate: 10 },
+    headers: {
+      "Content-Type": "application/json",
+      "X-MICROCMS-API-KEY": process.env.MICRO_CMS_API_KEY || "",
+    },
+    cache: "force-cache",
+  });
+
+  return await res.json();
+};
+
+export default async function HomePage() {
+  const res: Response = await getPopularBlogsData();
+
+  return <Home articles={{ popular: res.contents }} />;
 }
