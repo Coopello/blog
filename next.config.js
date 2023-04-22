44611f3bab44271c3fd21af7370966fe04cd753e
Kaito-Dogi <kendobu0405@gmail.com>
2023-04-22 19:23:01 +0900

    🔧

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
   images: {
     domains: ["placehold.jp", "images.microcms-assets.io"],
   },
+  typescript: {
+    // Next の fetch が node の fetch と認識されてビルドエラーが起きるため、それを防止する
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
