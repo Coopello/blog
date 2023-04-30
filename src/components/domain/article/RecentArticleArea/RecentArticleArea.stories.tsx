a8fab926c387ec34f924182aa0e3db89dd800034
github akito-10 <akt.fukuda@gmail.com>
2023-04-30 14:05:41 +0900

    ✅

--- src/components/domain/article/RecentArticleArea/RecentArticleArea.stories.tsx
+++ src/components/domain/article/RecentArticleArea/RecentArticleArea.stories.tsx
@@ -6,4 +6,40 @@ export default { title: "domain/article/RecentArticleArea" } as ComponentMeta<
   typeof RecentArticleArea
 >;
 
-export const Initial: ComponentStory<typeof RecentArticleArea> = (args) => <RecentArticleArea {...args} />;
+export const Initial: ComponentStory<typeof RecentArticleArea> = (args) => (
+  <RecentArticleArea {...args} />
+);
+
+Initial.args = {
+  articles: {
+    contents: [
+      {
+        id: "1",
+        title: "タイトル",
+        description: "記事の説明",
+        content: "記事の本文",
+        thumbnail: {
+          url: "https://example.com/thumbnail.jpg",
+          height: 200,
+          width: 300,
+        },
+        categories: ["カテゴリ1", "カテゴリ2"],
+        tags: [{ name: "タグ1" }, { name: "タグ2" }, { name: "タグ3" }],
+        author: {
+          id: "1",
+          createdAt: "2022-01-01T00:00:00.000Z",
+          updatedAt: "2022-01-02T00:00:00.000Z",
+          publishedAt: "2022-01-03T00:00:00.000Z",
+          revisedAt: "2022-01-04T00:00:00.000Z",
+          name: "著者名",
+          job: "職業",
+          description: "著者の自己紹介",
+          github: "https://github.com/example",
+          twitter: "https://twitter.com/example",
+          icon: "https://example.com/icon.jpg",
+        },
+      },
+    ],
+    totalCount: 1,
+  },
+};
--- src/components/domain/article/RecentArticleArea/RecentArticleArea.test.tsx
+++ src/components/domain/article/RecentArticleArea/RecentArticleArea.test.tsx
@@ -1,9 +1,45 @@
 import renderer from "react-test-renderer";
+import type { Article } from "src/models/article";
+
 import { RecentArticleArea } from "./";
+
+const dummyNoImageArticle: Article = {
+  id: "1",
+  title: "タイトル",
+  description: "記事の説明",
+  content: "記事の本文",
+  thumbnail: {
+    url: "https://example.com/thumbnail.jpg",
+    height: 200,
+    width: 300,
+  },
+  categories: ["カテゴリ1", "カテゴリ2"],
+  tags: [{ name: "タグ1" }, { name: "タグ2" }, { name: "タグ3" }],
+  author: {
+    id: "1",
+    createdAt: "2022-01-01T00:00:00.000Z",
+    updatedAt: "2022-01-02T00:00:00.000Z",
+    publishedAt: "2022-01-03T00:00:00.000Z",
+    revisedAt: "2022-01-04T00:00:00.000Z",
+    name: "著者名",
+    job: "職業",
+    description: "著者の自己紹介",
+    github: "https://github.com/example",
+    twitter: "https://twitter.com/example",
+    icon: "https://example.com/icon.jpg",
+  },
+};
 
 describe("domain/article/RecentArticleArea", () => {
   it("Snap Shot", () => {
-    const component = renderer.create(<RecentArticleArea />);
+    const component = renderer.create(
+      <RecentArticleArea
+        articles={{
+          contents: [dummyNoImageArticle],
+          totalCount: 1,
+        }}
+      />
+    );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
