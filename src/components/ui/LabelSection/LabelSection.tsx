7bf80280eb1799b7388b2b819d6e6c75352f792b
Kaito-Dogi <kendobu0405@gmail.com>
2023-05-25 23:18:37 +0900

    💄

--- src/components/ui/LabelSection/LabelSection.tsx
+++ src/components/ui/LabelSection/LabelSection.tsx
@@ -20,8 +20,8 @@ const styles: {
       gap: "1rem",
     },
     heading: {
-      fontSize: "1.5rem",
-      lineHeight: "2rem",
+      // fontSize: "1.5rem",
+      // lineHeight: "2rem",
       borderLeftWidth: 8,
     },
   },
@@ -31,8 +31,8 @@ const styles: {
       gap: "1rem",
     },
     heading: {
-      fontSize: "1.5rem",
-      lineHeight: "2rem",
+      // fontSize: "1.5rem",
+      // lineHeight: "2rem",
       borderLeftWidth: 8,
     },
   },
@@ -46,7 +46,7 @@ export const LabelSection: FC<Props> = ({ children, label, size = "md" }) => {
     <section style={styles[size].section} className="flex w-full flex-col">
       <h2
         style={styles[size].heading}
-        className="border-custom-primary px-2 py-1 text-custom-black sm:px-4 sm:py-2"
+        className="border-custom-primary px-3 py-1 text-custom-black sm:px-4 sm:py-2"
       >
         {label}
       </h2>
