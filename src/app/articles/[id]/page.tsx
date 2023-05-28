fc1a04af492d6e2186e1bd66bb14cca4a39f1a3c
github akito-10 <akt.fukuda@gmail.com>
2023-05-28 14:48:43 +0900

    ✨ dynamic metadata
    Co-authored-by: Doggy <Kaito-Dogi@users.noreply.github.com>

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,71 +1,24 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-import { notFound } from "next/navigation";
+import {
+  getArticleDetail,
+  getRecommendArticles,
+} from "src/app/articles/[id]/requests";
 import { Detail } from "src/components/page/Detail";
-import type { Article } from "src/models/article";
 
-type Response = {
-  contents: Article[];
-  totalCount: number;
-};
-
-const getArticleDetail = async (articleId: string) => {
-  const res = await fetch(
-    `${process.env.MICRO_CMS_API_URL}/articles/${articleId}`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        "X-MICROCMS-API-KEY": process.env.MICRO_CMS_API_KEY || "",
-      },
-      cache: "force-cache",
-      next: { revalidate: 10 },
-    }
-  );
-
-  if (res.status === 404) {
-    notFound();
-  }
-
-  return await res.json();
-};
-
-const getRecommendArticles = async (articleTagIds: string[]) => {
-  const filtersConditions = articleTagIds.reduce(
-    (filtersConditions, currentId, i) => {
-      if (i < articleTagIds.length - 1) {
-        return filtersConditions + `tags[contains]${currentId}[or]`;
-      }
-
-      return filtersConditions + `tags[contains]${currentId}`;
-    },
-    ""
-  );
-
-  const res = await fetch(
-    `${process.env.MICRO_CMS_API_URL}/articles?orders=-pv&limit=3&filters=${filtersConditions}`,
-    {
-      headers: {
-        "Content-Type": "application/json",
-        "X-MICROCMS-API-KEY": process.env.MICRO_CMS_API_KEY || "",
-      },
-      next: { revalidate: 10 },
-      cache: "force-cache",
-    }
-  );
-
-  if (res.status === 404) {
-    notFound();
-  }
-
-  return await res.json();
-};
-
-export default async function DetailPage({
-  params,
-}: {
+type PageProps = {
   params: { id: string };
-}) {
-  const article: Article = await getArticleDetail(params.id);
-  const recommendArticlesResponse: Response = await getRecommendArticles(
+};
+
+export const generateMetadata = async ({ params }: PageProps) => {
+  const article = await getArticleDetail(params.id);
+
+  return {
+    title: `${article.title} - Coopello Blog`,
+  };
+};
+
+export default async function DetailPage({ params }: PageProps) {
+  const article = await getArticleDetail(params.id);
+  const recommendArticlesResponse = await getRecommendArticles(
     article.tags.map((tag) => {
       return tag.id;
     })
---
+++ src/app/articles/[id]/requests.ts
@@ -0,0 +1,60 @@
+import { notFound } from "next/navigation";
+import type { Article } from "src/models/article";
+
+type Response = {
+  contents: Article[];
+  totalCount: number;
+};
+
+export const getArticleDetail = async (articleId: string): Promise<Article> => {
+  const res = await fetch(
+    `${process.env.MICRO_CMS_API_URL}/articles/${articleId}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "X-MICROCMS-API-KEY": process.env.MICRO_CMS_API_KEY || "",
+      },
+      cache: "force-cache",
+      next: { revalidate: 10 },
+    }
+  );
+
+  if (res.status === 404) {
+    notFound();
+  }
+
+  return await res.json();
+};
+
+export const getRecommendArticles = async (
+  articleTagIds: string[]
+): Promise<Response> => {
+  const filtersConditions = articleTagIds.reduce(
+    (filtersConditions, currentId, i) => {
+      if (i < articleTagIds.length - 1) {
+        return filtersConditions + `tags[contains]${currentId}[or]`;
+      }
+
+      return filtersConditions + `tags[contains]${currentId}`;
+    },
+    ""
+  );
+
+  const res = await fetch(
+    `${process.env.MICRO_CMS_API_URL}/articles?orders=-pv&limit=3&filters=${filtersConditions}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "X-MICROCMS-API-KEY": process.env.MICRO_CMS_API_KEY || "",
+      },
+      next: { revalidate: 10 },
+      cache: "force-cache",
+    }
+  );
+
+  if (res.status === 404) {
+    notFound();
+  }
+
+  return await res.json();
+};
