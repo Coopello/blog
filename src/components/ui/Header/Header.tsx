9951eb566f64fb7727041dc2369c5f204c550c8e
github akito-10 <akt.fukuda@gmail.com>
2024-02-23 00:37:07 +0900

    Refactor header component layout

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -7,11 +7,13 @@ import type { FC } from "react";
  */
 export const Header: FC = () => {
   return (
-    <header className="flex items-center gap-3 bg-custom-white p-3 sm:gap-4 sm:p-4">
-      <div className="relative h-10 w-10 sm:h-12 sm:w-12">
-        <Image src="/static/coopello_logo.svg" alt="こぺろロゴ" fill />
+    <header className="box-border bg-custom-white">
+      <div className="mx-auto box-content flex w-full max-w-[1484px] items-center bg-custom-white p-4 sm:w-[90%]">
+        <div className="relative mr-4 h-10 w-10 sm:h-12 sm:w-12">
+          <Image src="/static/coopello_logo.svg" alt="こぺろロゴ" fill />
+        </div>
+        <h1 className="text-2xl font-extralight sm:text-4xl">Coopello Blog</h1>
       </div>
-      <h1 className="text-2xl sm:text-3xl">Coopello Blog</h1>
     </header>
   );
 };
