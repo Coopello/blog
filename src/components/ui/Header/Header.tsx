bac585f80f2d44fe9c1a3c0b93fb66427629cf16
github akito-10 <akt.fukuda@gmail.com>
2024-02-23 00:50:06 +0900

    Update Header component styling

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -7,12 +7,12 @@ import type { FC } from "react";
  */
 export const Header: FC = () => {
   return (
-    <header className="box-border bg-custom-white">
-      <div className="mx-auto box-content flex w-full max-w-[1484px] items-center bg-custom-white p-4 sm:w-[90%]">
-        <div className="relative mr-4 h-10 w-10 sm:h-12 sm:w-12">
+    <header className="box-border overflow-hidden bg-custom-white">
+      <div className="mx-auto box-content flex w-full max-w-[1484px] items-center bg-custom-white p-2 sm:w-[90%] sm:py-6">
+        <div className="relative mr-2 h-6 w-6 sm:mr-4 sm:h-12 sm:w-12">
           <Image src="/static/coopello_logo.svg" alt="こぺろロゴ" fill />
         </div>
-        <h1 className="text-2xl font-extralight sm:text-4xl">Coopello Blog</h1>
+        <h1 className="text-xl font-extralight sm:text-3xl">Coopello Blog</h1>
       </div>
     </header>
   );
