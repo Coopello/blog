3f7a74ef527e1f0eb6fec01617585be7ea065902
github akito-10 <akt.fukuda@gmail.com>
2024-02-23 18:23:32 +0900

    Update linting command and header logo size

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -14,7 +14,7 @@ export const Header: FC = () => {
         className="mx-auto box-content flex w-full max-w-[1484px] items-center bg-custom-white p-2 sm:w-[90%] sm:py-6"
         href="/"
       >
-        <div className="relative mr-2 h-6 w-6 sm:mr-4 sm:h-12 sm:w-12">
+        <div className="relative mr-2 size-6 sm:mr-4 sm:size-12">
           <Image src="/static/coopello_logo.svg" alt="こぺろロゴ" fill />
         </div>
         <h1 className="text-xl font-extralight sm:text-3xl">Coopello Blog</h1>
