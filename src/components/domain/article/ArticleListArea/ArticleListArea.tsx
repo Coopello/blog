6ab513e70eaea8abbb26beebdbaef9fa59f27f32
github akito-10 <akt.fukuda@gmail.com>
2024-03-26 02:54:29 +0900

    refactor

--- src/components/domain/article/ArticleListArea/ArticleListArea.tsx
+++ src/components/domain/article/ArticleListArea/ArticleListArea.tsx
@@ -1,7 +1,5 @@
-import { useParams, useRouter } from "next/navigation";
 import { FC } from "react";
 import { ArticleCard } from "src/components/domain/article/ArticleCard";
-import { Button } from "src/components/ui/Button";
 import { Article } from "src/models/article";
 
 type Props = {
@@ -12,15 +10,11 @@ type Props = {
 /**
  * @package
  */
-export const ArticleListArea: FC<Props> = ({ articles, totalCount }) => {
-  const router = useRouter();
-  const params = useParams();
-  const page = params.page ? +params.page : 1;
-
+export const ArticleListArea: FC<Props> = ({ articles }) => {
   return (
     <section className="flex flex-col gap-6 items-center">
       <ul className="grid grid-cols-1 gap-6">
-        {articles?.map((article) => (
+        {articles.map((article) => (
           <li key={article.id}>
             <ArticleCard
               key={article.id}
