f8b92b5811af0b9593d5d5b00e36b51ea2930892
github akito-10 <akt.fukuda@gmail.com>
2024-03-26 02:52:41 +0900

    api routes

---
+++ src/request/getArticlesFromType.ts
@@ -0,0 +1,30 @@
+import type { Article } from "src/models/article";
+import { REVALIDATE_TIME } from "src/utils/constants";
+
+const PAGE_LIMIT = 100;
+
+/**
+ * @package
+ */
+export type ArticlesFromTypeResponse = {
+  contents: Article[];
+  totalCount: number;
+};
+
+/**
+ * @package
+ */
+export const getArticlesFromType = async (type: string, page: string) => {
+  const res = await fetch(
+    `${process.env.MICRO_CMS_API_URL}/articles?${type !== "" ? `filters=type[contains]${type}` : ""}&limit=${PAGE_LIMIT}&offset=${(Number(page) - 1) * PAGE_LIMIT}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "X-MICROCMS-API-KEY": process.env.MICRO_CMS_API_KEY || "",
+      },
+      next: { revalidate: REVALIDATE_TIME },
+    }
+  );
+
+  return await res.json();
+};
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -9,3 +9,7 @@ export type MicroCmsImage = {
 export type DetailPageProps = {
   params: { id: string; type: Article["type"] };
 };
+
+export type ListTypePageProps = {
+  params: { type: "events" | "slides" | "tech" | ""; page: string | undefined };
+};
